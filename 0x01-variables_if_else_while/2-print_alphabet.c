6c459c2211b30a0244f6a9722e8b1f7f8cbe1265
alexander083 <arojas083@gmail.com>
2020-06-12 02:42:02 +0000

    FILE

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+/**
+ * *main - Print the last digit and do an if evaluation
+ * *
+ * *Return: Always Zero
+ *
+ *
+ */
+#include <stdio.h>
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c = c + 1;
+}
+putchar(10);
+return (0);
+}
