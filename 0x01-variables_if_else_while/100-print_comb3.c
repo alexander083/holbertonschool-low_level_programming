0e229c30678ac8d9d212d292d8d76de308ffcac8
alexander083 <arojas083@gmail.com>
2020-06-13 01:13:06 +0000

    File11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+/**
+ * *main-Escriba un programa que imprima todas las combinaciones
+ * *posibles de números de un dos dígito.
+ * *separados por ,, seguidos de un espacio
+ * *Sin repetir
+ * *Return: Always Zero
+ */
+#include <stdio.h>
+int main(void)
+{
+int c = 48;
+int d;
+while (c <= 57)
+{
+d = 49;
+while (d <= 57)
+{
+if (c != d  && c < d)
+{
+putchar(c);
+putchar(d);
+if (c != 56 || d != 57)
+putchar(44);
+putchar(32);
+}
+d = d + 1;
+}
+c = c + 1;
+}
+putchar(10);
+return (0);
+}
