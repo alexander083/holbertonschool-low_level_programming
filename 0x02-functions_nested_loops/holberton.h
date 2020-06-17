2d66266dbaebf0b9f5ec158c010ebe66434aa5d7
alexander083 <arojas083@gmail.com>
2020-06-17 20:56:59 +0000

    All

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,143 @@
+/**
+  * _putchar - writes the character c to stdout
+  * @c: The character to print
+  *
+  * Return: On success 1.
+  * On error, -1 is returned, and errno is set appropriately.
+  */
+int _putchar(char c);
+/** 
+  * alphabet printable function
+  */
+void print_alphabet(void);
+/**
+  *
+  * alphabet x10
+  *
+  */
+void print_alphabet_x10(void);
+/**
+  *
+  *is lower 
+  *
+  */
+int _islower(int c);
+/**
+  *
+  *check the code for Holberton School students.
+  *
+  */
+int _isalpha(int c);
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+int print_sign(int n);
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+int _abs(int);
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+int print_last_digit(int);
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+void jack_bauer(void);
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+void times_table(void);
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+int add(int, int);
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+void print_to_98(int n);/**
+			  * _putchar - writes the character c to stdout
+			  * @c: The character to print
+			  *
+			  * Return: On success 1.
+			  * On error, -1 is returned, and errno is set appropriately.
+			  */
+int _putchar(char c);
+/** 
+  * alphabet printable function
+  */
+void print_alphabet(void);
+/**
+  *
+  * alphabet x10
+  *
+  */
+void print_alphabet_x10(void);
+/**
+  *
+  *is lower 
+  *
+  */
+int _islower(int c);
+/**
+  *
+  *check the code for Holberton School students.
+  *
+  */
+int _isalpha(int c);
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+int print_sign(int n);
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+int _abs(int);
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+int print_last_digit(int);
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+void jack_bauer(void);
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+void times_table(void);
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+int add(int, int);
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+void print_to_98(int n);
