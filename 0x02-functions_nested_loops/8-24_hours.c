9ff11ea240ae3b4b7e8ff3b613c7c7586a5efd67
alexander083 <arojas083@gmail.com>
2020-06-19 01:53:56 +0000

    file 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,26 @@
-m
+#include "holberton.h"
+/**
+  * jack_bauer - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+void jack_bauer(void)
+{
+	int H = 0, M;
+
+	while (H <= 23)
+	{
+		M = 0;
+		while (M <= 59)
+		{
+			_putchar (H / 10 + '0');
+			_putchar (H % 10 + '0');
+			_putchar (':');
+			_putchar (M / 10 + '0');
+			_putchar (M % 10 + '0');
+			_putchar (10);
+			M++;
+		}
+		H++;
+	}
+}
